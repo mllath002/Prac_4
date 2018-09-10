c95eb885444171b681eef6a11f7c7a23a9558d45
mllath002 <43150701+mllath002@users.noreply.github.com>
2018-09-10 23:28:36 +0200

    Update Prac_4.py

--- Prac_4.py
+++ Prac_4.py
@@ -1,23 +1,28 @@
 # -*- coding: cp1252 -*-
 import RPi.GPIO as GPIO
 import Adafruit_MCP3008
+import spidev 
 import time
 import os
 import sys
 
 GPIO.setmode(GPIO.BCM) #pin numbering BCM
 
-#pin definition
+# pin definition
+# for SPI pin 9-11
 SPICLK = 11
 SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
-pot_pin_number = #potentiometer 1K 
-temp_pin_number = # temperature sensor 
-ldr_pin_number = # detect light 
-reset_button = 17 #switch resets timer and cleans console 
-freq_button = 26 #switch to change frequency
-stop_button = 23 #switch off or on monitoring. Display when off.
+
+reset_button = 17 # switch resets timer and cleans console 
+freq_button = 26 # switch to change frequency
+stop_button = 23 # switch off or on monitoring. Display when off.
+
+# sensors channel 0-7 definition MCP3008
+light_channel = 0
+temp_channel = 1 
+pot_channel = 2
 
 # set outputs 
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -38,43 +43,39 @@ bouncetime=200)
 GPIO.add_event_detect(stop_button, GPIO.FALLING, callback=callback_stop,
 bouncetime=200)
 
+# Open SPI bus
+spi = spidev.SpiDev() # create spi object
+spi.open(0,0)
 
+# Define delay between readings
+delay = .5
+
+# what this line does, i have no clue. 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
 # reset_button_pressed = True
 # freq_button_pressed = True
 # stop_button_pressed = True
 
-#global variable ?????
-values = [0]*8
+# global variable ?????
+# values = [0]*8
 
-while True:
-for i in range(8):
-values[i] = mcp.read_adc(i)
- # delay for a half second
- time.sleep(0.5)
-print values
-
-# Open SPI bus
-spi = spidev.SpiDev() # create spi object
-spi.open(0,0)
-# RPI has one bus (#0) and two devices (#0 & #1)
-# function to read ADC data from a channel
-
-# Define sensor channels
-channel = 0
-# Define delay between readings
-delay = .5
 try:
 while True:
-# Read the data
- sensr_data = GetData (channel)
+# read light sensor data 
+ sensor_data = GetData (light_channel)
  sensor_volt = ConvertVolts(sensor_data,2)
-
+# read temp sensor data 
+ sensor_data = GetData (temp_channel)
+ sensor_volt = ConvertVolts(sensor_data,2)
+# read pot sensor data 
+ sensor_data = GetData (pot_channel)
+ sensor_volt = ConvertVolts(sensor_data,2)
 # Wait before repeating loop
  time.sleep(delay)
 except KeyboardInterrupt:
- spi.close()
+ spi.close()
+
 
 
 # FUNCTION DEFINITION: threaded callback
@@ -89,8 +90,16 @@ def callback_stop(channel):
 # then display first five readings when stop switch pressed 
 
 
-# FUNCTION DEFINITION: get data from channel
-def GetData(channel): # channel must be an integer 0-7
+# FUNCTION DEFINITION: get data from channels 
+def GetData(0): # light_channel
+ adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
+ data = ((adc[1]&3) << 8) + adc[2]
+ return data
+def GetData(1): # temp_channel
+ adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
+ data = ((adc[1]&3) << 8) + adc[2]
+ return data
+def GetData(2): # pot_channel
  adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
  data = ((adc[1]&3) << 8) + adc[2]
  return data
@@ -98,18 +107,22 @@ def GetData(channel): # channel must be an integer 0-7
 # FUNCTION DEFINITION: convert data to voltage 
 def ConvertVolts(data,places): # places: number of decimal places needed
  volts = (data * 3.3) / float(1023)
- volts = round(volts,places)
+ volts = round(volts,2) #round of to two decimal places
  return volts
 
 # 'bouncetime=200' includes the bounce control
-# ‘bouncetime=200’ sets 200 milliseconds during which second button press will
-#  be ignored.
+# â€˜bouncetime=200â€™ sets 200 milliseconds during which second button press will be ignored.
 # to remove: GPIO.remove_event_detect(port_number)
 
+# FUNCTION DEFINITION: convert data to temperature 
+def ConvertTemp(data): 
+ temp = ((data*330/float(1023))-50
+ temp = round(temp,0) #round to zero decimal places 
+ return temp #return temp in degrees 
 
+# try:
+# GPIO.wait_for_edge(switch_3, GPIO.RISING)
+# except KeyboardInterrupt:
+# GPIO.cleanup() # clean up GPIO on CTRL+C exit
+# GPIO.cleanup() # clean up GPIO on normal exit
 
-try:
-GPIO.wait_for_edge(switch_3, GPIO.RISING)
-except KeyboardInterrupt:
-GPIO.cleanup() # clean up GPIO on CTRL+C exit
-GPIO.cleanup() # clean up GPIO on normal exit
